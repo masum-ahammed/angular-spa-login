a1160d0e6c09994d320bbe7211b1438a5e6b00d1
Masum Ahammed <masum.ahammed@gmail.com>
2016-03-08 11:13:25 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,33 @@
+(function () {
+    'use strict';
+
+    var app = angular.module('app', [
+        // Angular modules 
+        'ngAnimate',        // animations
+        'ngRoute',          // routing
+        'ngSanitize',       // sanitizes html bindings (ex: sidebar.js)
+
+
+        // 3rd Party Modules
+        //'ui.bootstrap'      // ui-bootstrap (ex: carousel, pagination, dialog)
+    ]);
+
+    app.config(['$routeProvider',
+        function ($routeProvider) {
+            $routeProvider.
+                when('/', {
+                    templateUrl: 'js/login/login.html',
+                    controller: 'loginController',
+                    controllerAs: 'vm'
+                }).
+
+                otherwise({
+                    redirectTo: '/'
+                });
+        }]);
+    // Handle routing errors and success events
+    app.run(['$route', function ($route) {
+        // Include $route to kick start the router.
+    }]);
+   
+})();
---
+++ js/login/loginController.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('loginController', loginController);
+
+    loginController.$inject = ['$location']; 
+
+    function loginController($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'loginController';
+
+        vm.submitForm = function () {
+
+        }
+
+       
+    }
+})();
