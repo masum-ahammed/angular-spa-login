278cbfea1985056c8a326f974917fa865c11b74e
Masum Ahammed <masum.ahammed@gmail.com>
2016-03-08 12:21:03 +0600

    show shake animation during login failed

---
+++ js/login/login.controller.js
@@ -0,0 +1,40 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('loginController', loginController);
+
+    loginController.$inject = ['loginService', '$animate', '$timeout'];
+
+    function loginController(loginService, $animate, $timeout) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'loginController';
+
+        vm.submitForm = function (isValid) {
+            if (!isValid) {
+                shakeForm();
+                return false;
+            }
+
+            loginService.login(vm.email, vm.password).then(function (user) {
+                if (!user) {
+                    vm.errorMsg = 'Login failed.';
+                    shakeForm();
+                }
+              
+            });
+        }
+
+        function shakeForm() {
+            var element = angular.element('.login-form');
+            element.removeClass('shake');
+            $animate.addClass(element, 'shake', function () {
+                $animate.removeClass(element, 'shake');
+            });
+        }
+
+       
+    }
+})();
---
+++ js/login/login.service.js
@@ -0,0 +1,38 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('loginService', loginService);
+
+    loginService.$inject = ['$http','$q'];
+
+    function loginService($http, $q) {
+        var _User;
+        var service = {
+            login: login,
+            getLogedInUser: getLogedInUser
+        };
+
+        return service;
+
+        function login(email, password) {
+            var deferred = $q.defer();
+
+            $http.get('js/data/data.json').success(function (data) {
+                var user = data.find(function (user) {
+                    return user.email == email && user.password == password
+                });
+                _User = user;
+                deferred.resolve(user);
+            })
+            .error(function (msg) {
+                deferred.reject(msg);
+            });
+            return deferred.promise;
+        }
+        function getLogedInUser() {
+            return _User;
+        }
+    }
+})();
